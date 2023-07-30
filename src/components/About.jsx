648e305264cbfc68fbeb844a735026eb44909e7b
AMx14 <maithaniaxat@gmail.com>
2023-07-31 00:49:32 +0530

    Changed About part

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,7 @@ const ServiceCard = ({ index, title, icon ,description}) => {
     const [isHovered, setIsHovered] = useState(false); // State to keep track of the hover state
 
     return (
-        <Tilt className='xs:w-[250px] w-full'>
+        <Tilt className='xs:w-[350px] w-full'>
             <motion.div
                 variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
                 className='w-full bg-gradient-to-r from-orange-500 from-10% via-black via-30% to-yellow-500 to-90% ... p-[1px] rounded-[20px] shadow-card'
@@ -26,7 +26,7 @@ const ServiceCard = ({ index, title, icon ,description}) => {
                     }}
                     className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
                 >
-                    <img src={icon} alt='web-development' className='w-16 h-16 object-contain' />
+                    <img src={icon} alt='web-development' className='w-12 h-12 object-contain' />
 
                     <h3 className='text-white text-[20px] font-bold text-center'>{title}</h3>
                     <h3 className='text-white text-[10px] font-bold text-center'>
