fd046a512ed2199946dc8f0508e11f6e1ffbcad3
Shourya Maithani <smaithani010@gmail.com>
2023-07-31 14:13:39 +0400

    edit 3d model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,7 +5,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./desktop_pc/scene.gltf");
+  const computer = useGLTF("./3dmod/scene.gltf");
 
   return (
     <mesh>
@@ -21,9 +21,9 @@ const Computers = ({ isMobile }) => {
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3.5, -2.2] : [0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
+        scale={isMobile ? 0.000050 : 0.00012}
+        position={isMobile ? [0, -3.5, -2.2] : [0, -3, -1.5]}
+        rotation={[0.0, 0, 0]}
       />
     </mesh>
   );
