b5f56ea75315d52cd7fe4e275757edec0ffd8ba7
AMx14 <maithaniaxat@gmail.com>
2023-08-01 12:18:41 +0530

    Changed colours & looks

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,11 +7,8 @@ import { styles } from '../styles';
 import { EarthCanvas } from './canvas';
 import { SectionWrapper } from '../hoc';
 import { slideIn } from '../utils/motion';
-//import { FaLinkedin,} from "react-icons/fa";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLinkedin} from "@fortawesome/free-brands-svg-icons";
-import codeforcesIcon from '/src/assets/code-forces.svg';
-import leetcodeIcon from '/src/assets/leetcode.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 
 
@@ -74,64 +71,92 @@ const Contact = () => {
       );
   };
 
-  return (
-    <div className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}>
-      <motion.div variants={slideIn("left", "tween", 0.2, 1)} className='flex-[0.75] bg-black-100 p-8 rounded-2xl'>
-        <p className={styles.sectionSubText}>Get in touch</p>
-        <h3 className={styles.sectionHeadText}>Contact.</h3>
+    return (
+        <div className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}>
+            <motion.div
+                variants={slideIn("left", "tween", 0.2, 1)}
+                className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
+                style={{ backgroundColor: '#151A30' }} // Replace 'YOUR_COLOR_HERE' with the desired color value
+            >
+                <p className={styles.sectionSubText}>Get in touch</p>
 
-        
+                {/* Social Media Icons */}
+                <div className='flex gap-4 mb-4'>
+                    {/* Replace "YOUR_FACEBOOK_PROFILE_URL" with your actual Facebook profile URL */}
+                    <a href='YOUR_FACEBOOK_PROFILE_URL' target='_blank' rel='noopener noreferrer'>
+                        <FontAwesomeIcon icon={faFacebook} size='2x' className='text-white hover:text-blue-500' />
+                    </a>
+                    {/* Replace "YOUR_LINKEDIN_PROFILE_URL" with your actual LinkedIn profile URL */}
+                    <a href='YOUR_LINKEDIN_PROFILE_URL' target='_blank' rel='noopener noreferrer'>
+                        <FontAwesomeIcon icon={faLinkedin} size='2x' className='text-white hover:text-blue-500' />
+                    </a>
+                    {/* Replace "YOUR_TWITTER_PROFILE_URL" with your actual Twitter profile URL */}
+                    <a href='YOUR_TWITTER_PROFILE_URL' target='_blank' rel='noopener noreferrer'>
+                        <FontAwesomeIcon icon={faTwitter} size='2x' className='text-white hover:text-blue-500' />
+                    </a>
+                </div>
 
-        <form ref={formRef} onSubmit={handleSubmit} className='mt-12 flex flex-col gap-8'>
-          <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your Name</span>
-            <input
-              type='text'
-              name='name'
-              value={form.name}
-              onChange={handleChange}
-              placeholder="What's your good name?"
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
-            />
-          </label>
-          <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your email</span>
-            <input
-              type='email'
-              name='email'
-              value={form.email}
-              onChange={handleChange}
-              placeholder="What's your web address?"
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
-            />
-          </label>
-          <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your Message</span>
-            <textarea
-              rows={7}
-              name='message'
-              value={form.message}
-              onChange={handleChange}
-              placeholder='What you want to say?'
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
-            />
-          </label>
+                <h3 className={styles.sectionHeadText}>Contact.</h3>
 
-          <button type='submit' className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'>
-            {loading ? "Sending..." : "Send"}
-          </button>
-        </form>
-        
-      </motion.div>
-      
-      <motion.div
-        variants={slideIn("right", "tween", 0.2, 1)}
-        className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
-      >
-        <EarthCanvas />
-      </motion.div>
-    </div>
-  );
+                <form ref={formRef} onSubmit={handleSubmit} className='mt-12 flex flex-col gap-8 flex-1'>
+                    <label className='flex flex-col'>
+                        <span className='text-white font-medium mb-4'>Your Name</span>
+                        <input
+                            type='text'
+                            name='name'
+                            value={form.name}
+                            onChange={handleChange}
+                            placeholder="What's your good name?"
+                            style={{ backgroundColor: 'whitesmoke' }}
+                            className='py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+                        />
+                    </label>
+                    <label className='flex flex-col'>
+                        <span className='text-white font-medium mb-4'>Your email</span>
+                        <input
+                            type='email'
+                            name='email'
+                            value={form.email}
+                            onChange={handleChange}
+                            placeholder="What's your web address?"
+                            style={{ backgroundColor: 'whitesmoke' }}
+                            className='py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+                        />
+                    </label>
+                    <label className='flex flex-col flex-1'>
+                        <span className='text-white font-medium mb-4'>Your Message</span>
+                        <textarea
+                            rows={7}
+                            name='message'
+                            value={form.message}
+                            onChange={handleChange}
+                            placeholder='What you want to say?'
+                            style={{ backgroundColor: 'whitesmoke' }}
+                            className='py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+                        />
+                    </label>
+
+                    {/* The following wrapper div ensures that the button is centered at the bottom */}
+                    <div className='flex justify-center items-center'>
+                        <button
+                            type='submit'
+                            className='py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary hover:bg-gray-700 transition-colors'
+                        >
+                            {loading ? 'Sending...' : 'Send'}
+                        </button>
+                    </div>
+                </form>
+            </motion.div>
+
+            <motion.div
+                variants={slideIn("right", "tween", 0.2, 1)}
+                className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
+            >
+                <EarthCanvas />
+            </motion.div>
+        </div>
+    );
+
 };
 
 export default SectionWrapper(Contact, "contact");
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -9,31 +9,26 @@ import { experiences } from "../constants";
 import { SectionWrapper } from "../hoc";
 import { textVariant } from "../utils/motion";
 
+// Update ExperienceCard component
 const ExperienceCard = ({ experience }) => {
     return (
         <VerticalTimelineElement
             contentStyle={{
-                background: "#b35c38",
+                background: "#204060", // Use a dark blue color for the content background
                 color: "#fff",
+                boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
             }}
             contentArrowStyle={{ borderRight: "7px solid  #232631" }}
-            iconStyle={{ background: experience.iconBg }}
-            icon={
-                <div className='flex justify-center items-center w-full h-full'>
-                    {experience.counter}
-                </div>
-            }
+            iconStyle={{ background: "#2A4365" }} // Use a slightly lighter blue for the icon background
+            icon={<div className="flex justify-center items-center w-full h-full">{experience.counter}</div>}
         >
             <div>
-                <h3 className='text-white text-[24px] font-bold text-centre justify-center'>{experience.title}</h3>
+                <h3 className="text-white text-2xl font-bold text-center mb-2">{experience.title}</h3>
             </div>
 
-            <ul className='mt-5 list-disc ml-5 space-y-2' style={{ listStyleType: "none" }}>
+            <ul className="mt-2 list-disc ml-5 space-y-2" style={{ listStyleType: "none" }}>
                 {experience.points.map((point, index) => (
-                    <li
-                        key={`experience-point-${index}`}
-                        className='text-secondary text-[14px] pl-1 tracking-wider justify-center'
-                    >
+                    <li key={`experience-point-${index}`} className="text-secondary text-lg pl-1">
                         {point}
                     </li>
                 ))}
@@ -42,25 +37,19 @@ const ExperienceCard = ({ experience }) => {
     );
 };
 
+// Update Experience component
 const Experience = () => {
     return (
         <>
             <motion.div variants={textVariant()}>
-                <p className={`${styles.sectionSubText} text-center`}>
-                    How we map out a client-oriented solution
-                </p>
-                <h2 className={`${styles.sectionHeadText} text-center`}>
-                    Steps
-                </h2>
+                <p className={`${styles.sectionSubText} text-center`}>How we map out a client-oriented solution</p>
+                <h2 className={`${styles.sectionHeadText} text-center text-4xl font-bold mt-4`}>Steps</h2>
             </motion.div>
 
-            <div className='mt-20 flex flex-col'>
-                <VerticalTimeline>
+            <div className="mt-20 flex flex-col items-center">
+                <VerticalTimeline layout="1-column">
                     {experiences.map((experience, index) => (
-                        <ExperienceCard
-                            key={`experience-${index}`}
-                            experience={experience}
-                        />
+                        <ExperienceCard key={`experience-${index}`} experience={experience} />
                     ))}
                 </VerticalTimeline>
             </div>
